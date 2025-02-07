8dc76e18beebe27e7f1736e774d2b4df087959d1
Amiru Ushan <152049636+amiruushan@users.noreply.github.com>
2025-02-08 01:10:51 +0530

    chat screen basic structure

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -22,7 +22,142 @@ class ChatPage extends StatelessWidget {
         ],
 
       ),
-      body: Container()
+      body: ListView(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            margin: EdgeInsets.all(50),
+            decoration: BoxDecoration(
+              color: Colors.grey[800],
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                  bottomLeft: Radius.circular(20)
+              ),
+            ),
+
+            child: Text(
+              "This is your msg",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+            ),
+
+          ),
+          Container(
+            padding: EdgeInsets.all(20),
+            margin: EdgeInsets.all(50),
+            decoration: BoxDecoration(
+              color: Colors.grey[800],
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                  bottomLeft: Radius.circular(20)
+              ),
+            ),
+
+            child: Text(
+              "This is your msg",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+            ),
+
+          ),
+          Container(
+            padding: EdgeInsets.all(20),
+            margin: EdgeInsets.all(50),
+            decoration: BoxDecoration(
+              color: Colors.grey[800],
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                  bottomLeft: Radius.circular(20)
+              ),
+            ),
+
+            child: Text(
+              "This is your msg",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+            ),
+
+          ),
+          Container(
+            padding: EdgeInsets.all(20),
+            margin: EdgeInsets.all(50),
+            decoration: BoxDecoration(
+              color: Colors.grey[800],
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                  bottomLeft: Radius.circular(20)
+              ),
+            ),
+
+            child: Text(
+              "This is your msg",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+            ),
+
+          ),
+          Container(
+            padding: EdgeInsets.all(20),
+            margin: EdgeInsets.all(50),
+            decoration: BoxDecoration(
+              color: Colors.grey[800],
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                  bottomLeft: Radius.circular(20)
+              ),
+            ),
+
+            child: Text(
+              "This is your msg",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+            ),
+
+          ),
+
+          Container(
+            height: 100,
+            margin: EdgeInsets.only(bottom: 0),
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+            ),
+            child: Row(
+              children: [
+                IconButton(onPressed: (){
+
+                },
+                  icon: Icon(Icons.add),
+                  color: Colors.white,
+                ),
+
+                IconButton(onPressed: (){
+
+                },
+                  icon: Icon(Icons.send),
+                  color: Colors.white,
+                  padding: EdgeInsets.only(left: 300),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
